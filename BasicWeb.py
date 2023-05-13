84bfabc08d0a749e3d667bcf528d6267f43ab4a6
OskarProgrammer <130141327+OskarProgrammer@users.noreply.github.com>
2023-05-13 16:24:04 +0200

    Adding file BasicWeb.py

--- BasicWeb.py
+++ BasicWeb.py
@@ -1,45 +1,49 @@
-import os
+from os import system,chdir
 import argparse
-import pyautogui as gui
-import time
-
-class Operacje():
-    def __init__(self,name, path):
-        self.name = name
-        self.path = path
-    def goToPath(self):
-        os.chdir(self.path)
-    def createFolder(self):
-        os.system("mkdir {}".format(self.name))
-        os.chdir(self.name)
-    def makro(self):
-        gui.hotkey('ctrl', 'z')
-        gui.press('enter')
-    def createFiles(self):
-        list = ["index.html","script.js","style.css"]
-        for x in list:
-            os.system("copy con {}".format(x))
-            os.system("pause")
-            time.sleep(1)
-            self.makro()
-
-    def check(self):
-        os.system("tree")
+import sys
 
 
+parser = argparse.ArgumentParser(description="Creating basic web files")
 
+parser.add_argument("-n","--name",help="Name of the project",required=True,type=str)
 
-parser = argparse.ArgumentParser(description="Creating basic web files (script.js, index.html, style.css)", prog="BIOS ACTIV")
-
-parser.add_argument ("-n","--name",help="Name of the folder in which will be syntax",required=True,type=str)
-
-parser.add_argument ("-p","--path",help="Path to location where will be created folder",required=True,type=str)
+parser.add_argument("-p","--path",help="Path to the place of the project",required=True,type=str)
 
 args = parser.parse_args()
 
-obiekt = Operacje(args.name,args.path)
+class CreateBasic(object):
 
-obiekt.goToPath()
-obiekt.createFolder()
-obiekt.createFiles()
-# obiekt.check()
+    def __init__(self,name,path):
+        self._name = name
+        self._path = path
+        self.all()
+
+    def goToPath(self):
+        chdir(self._path)
+
+    def createFolder(self):
+        system("mkdir {}".format(self._name))
+        chdir(self._name)
+
+    def createFiles(self):
+        list = "index.html script.js style.css".split(" ")
+        try:
+            for x in list:
+                with open(x,"a")as file:
+                    if x == list[0]:
+                        file.write("\"\"\" That's your html file where you can write html marks\"\"\"")
+                    elif x==list[1]:
+                        file.write(f"//That's your javascript file")
+                    else:
+                        file.write(f"// Here you can add style to components")
+                    
+        except Exception as e:
+            print(f"Error: {e}")
+            system("pause")
+
+    def all(self):
+        self.goToPath()
+        self.createFolder()
+        self.createFiles()
+        
+obiekt = CreateBasic(args.name,args.path)
